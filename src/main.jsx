a186e04bed75f46495e9ce03d975611d9e344dd2
viktorlapshin <123vik3@gmail.com>
2025-11-12 21:16:23 +0300

    delete error icon excel 1

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 
+// Получаем base path из конфигурации Vite
+const basename = import.meta.env.BASE_URL;
+
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
